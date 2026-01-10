c47bef1600a895f70326f407fa415eeeeaf2cb90
GitHub git update Action <action@github.com>
2026-01-10 00:07:06 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/rusefi_generated_agtechpro-h7.h
+++ generated/controllers/generated/rusefi_generated_agtechpro-h7.h
@@ -2077,7 +2077,7 @@
 #define ts_show_wastegate_sensor true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI main.2026.01.09.agtechpro-h7.57316958"
+#define TS_SIGNATURE "rusEFI main.2026.01.10.agtechpro-h7.57316958"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
